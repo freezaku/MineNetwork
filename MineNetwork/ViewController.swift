d5f92cd00eca751e0127f7ae99513e8a14cf4bf9
freezaku <812029204@qq.com>
2016-12-21 13:50:05 -0800

    Initial Commit

---
+++ MineNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MineNetwork
+//
+//  Created by 徐鸿力 on 16/12/21.
+//  Copyright © 2016年 Honglix Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
