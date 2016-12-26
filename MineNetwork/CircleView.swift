39b770efb0f9750c0d7657c6c4f28bca33550f4e
freezaku <812029204@qq.com>
2016-12-26 14:01:39 -0800

    feed-ui design

--- MineNetwork/CircleView.swift
+++ MineNetwork/CircleView.swift
@@ -1,22 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  MineNetwork
 //
-//  Created by 徐鸿力 on 16/12/21.
+//  Created by 徐鸿力 on 16/12/23.
 //  Copyright © 2016年 Honglix Xu. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
-        super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         layer.cornerRadius = 3.0
     }
+
 }
